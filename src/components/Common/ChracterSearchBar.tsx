b5bf5dffd6eac126dc004d632abbf960f85d3deb
code-bebop <woonj2@gmail.com>
2022-05-01 02:20:04 +0900

    CharacterSearchBar 컴포넌트 구현 완료
    캐릭터 정보 페이지 최상단에서 캐릭터를 검색 가능한 "CharacterSearchBar" 컴포넌트의 구현을 완료했다.

---
+++ src/components/Common/ChracterSearchBar.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { useNicknameDispatch } from "../../context/nickname";
+
+const CharacterSearchBlock = styled.div`
+  height: 75px;
+  padding-left: 30px;
+  display: flex;
+  align-items: center;
+`;
+
+const CharacterSearchInput = styled.input`
+  width: 300px;
+  height: 40px;
+  padding-left: 15px;
+  background-color: ${({ theme }) => theme.mainTheme.color.default};
+  border: 1px solid ${({ theme }) => theme.mainTheme.color.white};
+  border-radius: 5px;
+  outline: none;
+  font: ${({ theme }) => theme.mainTheme.font.body};
+`;
+
+const CharacterSearchBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { nickname: locationNickname } = location.state as { nickname: string };
+  const [nickname, setNickname] = useState(locationNickname);
+  const dispatch = useNicknameDispatch();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(nickname);
+    dispatch({ type: "NICKNAME/UPDATE", payload: nickname });
+    navigate("/profile/item/equipment", { state: { nickname } });
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <CharacterSearchBlock>
+      <form onSubmit={onSubmit}>
+        <CharacterSearchInput
+          type="text"
+          onChange={onChange}
+          value={nickname}
+        />
+      </form>
+    </CharacterSearchBlock>
+  );
+};
+
+export default CharacterSearchBar;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import CharacterOverView from "./Common/CharacterOverview";
+import CharacterSearchBar from "./Common/ChracterSearchBar";
 import TabMenu from "./Common/TabMenu";
 
 const Profile = () => {
   return (
     <>
+      <CharacterSearchBar />
       <CharacterOverView />
       <TabMenu />
       <Outlet />
