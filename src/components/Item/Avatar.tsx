467cfa22a37d48ff16702dfb447055c420adeb17
code-bebop <woonj2@gmail.com>
2022-05-07 20:56:03 +0900

    useProfileQuery가 반환하는 data를 narrowing하는 함수 구현
    "useProfileQuery" 가 반환하는 profileQuery의 상태에 따라 narrowing 하던 부분을 "getProfileData" 라는 함수로 분리하였다.
    다만 "getProfileData" 함수의 반환값의 type이 `EquipmentResponse | JSX.Element` 인데, 이 때문에 "getProfileData" 함수의 반환값에 `if (!("result" in getProfileData(profileQuery))) {
      return data;
    }` 조건문을 사용해서 다시 type narrowing을 해주어야 한다.

--- src/components/Item/Avatar.tsx
+++ src/components/Item/Avatar.tsx
@@ -1,8 +1,17 @@
 import { useLocation } from "react-router-dom";
+import useProfileQuery from "../../hooks/useProfileQuery";
+import getProfileData from "../../lib/getProfileData";
 
 const Avatar = () => {
   const { state } = useLocation();
   const { nickname } = state as { nickname: string };
+  const profileQuery = useProfileQuery("avatar", nickname);
+
+  const data = getProfileData(profileQuery);
+
+  if (!("result" in data)) {
+    return data;
+  }
 
   return <p>{`Avatar ${nickname}`}</p>;
 };
--- src/components/Item/Equipment/Equipment.tsx
+++ src/components/Item/Equipment/Equipment.tsx
@@ -2,29 +2,20 @@ import { useLocation } from "react-router-dom";
 
 import useEquipmentEngraves from "../../../hooks/useEquipmentEngraves";
 import useProfileQuery from "../../../hooks/useProfileQuery";
+import getProfileData from "../../../lib/getProfileData";
 import EquipmentEnvgraveOverview from "./EquipmentEnvgraveOverview";
 import EquipmentList from "./EquipmentList";
 
 const Equipment = () => {
   const location = useLocation();
   const { nickname } = location.state as { nickname: string };
-  const { data, isLoading, isError } = useProfileQuery("equipment", nickname);
+  const profileQuery = useProfileQuery("equipment", nickname);
 
+  const data = getProfileData(profileQuery);
   const equipmentEngraves = useEquipmentEngraves(data);
 
-  if (isLoading) {
-    return <p>로딩 중 . . .</p>;
-  }
-  if (typeof data === "undefined") {
-    return <p>데이터가 undefined인 상태입니다.</p>;
-  }
-  if (isError || data.result === "Error") {
-    return (
-      <>
-        <p>에러 발생</p>
-        <p>{data.result_error}</p>
-      </>
-    );
+  if (!("result" in data)) {
+    return data;
   }
 
   return (
