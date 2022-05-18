611f1b7ef8168051874b3a892ecd43c5278c2c5a
code-bebop <57097064+code-bebop@users.noreply.github.com>
2022-05-18 17:09:51 +0900

    Feature/#18 (#19)
    * Skill 컴포넌트 구현 완료
    
    "Skill", "SkillList" 컴포넌트의 구현을 완료했다.
    
    * SkillList 컴포넌트 UI 수정
    
    - "SkillList" 컴포넌트에서 스킬의 레벨을 표시하게 변경했다.
    - "SkillList" 컴포넌트의 글씨 색 변경 및 테두리를 추가했다.
    
    * 검색된 캐릭터가 사용할 수 있는 스킬이 없는 경우 대응
    
    검색된 캐릭터가 사용할 수 있는 스킬이 없을 경우 "SkillLIst"에서 적절한 화면을 보여주도록 변경했다.

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -1,5 +1,25 @@
+import { useLocation } from "react-router-dom";
+import useProfileQuery from "../../hooks/useProfileQuery";
+import profileQueryAnalyzer from "../../lib/profileQueryAnalyzer";
+import { SkillResponse } from "../../type/skill";
+import SkillList from "./SkillList";
+
 const Skill = () => {
-  return <p>Skill</p>;
+  const { state } = useLocation();
+  const { nickname } = state as { nickname: string };
+  const profileQuery = useProfileQuery<SkillResponse>("skill", nickname);
+
+  const data = profileQueryAnalyzer(profileQuery);
+
+  if (!("result" in data)) {
+    return data;
+  }
+
+  return (
+    <>
+      <SkillList {...data} />
+    </>
+  );
 };
 
 export default Skill;
---
+++ src/type/skill.ts
@@ -0,0 +1,29 @@
+import { BasicResponse } from "./basicResponse";
+
+export interface SkillResponse extends BasicResponse {
+  result: string;
+  skillList: SkillList[] | string;
+}
+
+export interface SkillList {
+  name: string;
+  level: number;
+  type: string;
+  image: string;
+  tripod?: Array<SelectedTripodList[]>;
+  selectedTripodList?: SelectedTripodList[];
+  rune?: Rune;
+}
+
+export interface Rune {
+  name: string;
+  grade: string;
+  description: string;
+}
+
+export interface SelectedTripodList {
+  name: string;
+  description: string;
+  level: number;
+  image: string;
+}
