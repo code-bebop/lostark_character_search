5a1cf39df91f4dbd65d4936a4aa2dad906cc74f7
code-bebop <woonj2@gmail.com>
2022-06-03 07:11:37 +0900

    배포 완료
    배포를 완료하였다.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import Router from "./Router";
 import "./reset.css";
 import "./main.css";
@@ -12,12 +12,12 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <HashRouter basename={import.meta.env.PUBLIC_URL}>
       <ThemeProvider theme={mainTheme}>
         <React.StrictMode>
           <Router />
         </React.StrictMode>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 );
